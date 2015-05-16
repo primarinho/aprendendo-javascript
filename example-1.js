d7b5344bf495562795424db9e46a0a0e7703991a
Priscila Marinho <prislmarinho@gmail.com>
2015-05-16 20:40:17 -0300

    Add comments
    file comments

--- example-1.js
+++ example-1.js
@@ -1,7 +1,12 @@
+/* Hello World */
 console.log('Hello World');
 
+/* Exibindo na tela a soma de dois valores */
 console.log(1+1);
 
+/* criando três variáveis, variável a com o valor (1), variável b com o valor (2) e variável
+result somando os valores que existem em (a e b), usando console.log para exibir as
+somas pegando a variável (result) */
 var a = 1;
 var b = 2;
 var result = a+b;
--- example-2.js
+++ example-2.js
@@ -1,3 +1,10 @@
+/* 
+	Criando a função (cumprimentar), recebendo dois parâmetros (nome, sobrenome).
+	Criando a variável (frase) que está rebendo os dois parâmetros como variáveis.
+	Criando a closure (cumprimentarConsole) que exibe o conteúdo da variável (frase).
+	Fazendo a chamada da function (cumprimentarConsole).
+	Fazendo a chamada da function (cumprimentar) fornecendo os argumentos.
+*/
 function cumprimentar(nome, sobrenome) {
 	var frase = 'Olá ' + nome + ' ' + sobrenome + '! ';
 
